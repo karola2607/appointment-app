a51e1086d6fae769c000032a8a07e8aba126b7c0
Karolina Pastor <borek.karola@gmail.com>
2024-12-03 14:59:43 +0100

    Create an appointment interface

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
   standalone: false,
-  
+
   templateUrl: './appointment-list.component.html',
-  styleUrl: './appointment-list.component.css'
+  styleUrl: './appointment-list.component.css',
 })
 export class AppointmentListComponent {
-
+  appointment: Appointment = {
+    id: 1,
+    title: 'Take a dog for a walk',
+    date: new Date('2025-07-30'),
+  };
 }
